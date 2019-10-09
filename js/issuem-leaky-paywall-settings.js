d711c502b9f268be4475ed075728bd543fa07e6f
Gabriel Comte <comte@puzzle.ch>
2019-10-09 21:29:15 +0200

    further cleaning

--- js/issuem-leaky-paywall-settings.js
+++ js/issuem-leaky-paywall-settings.js
@@ -12,61 +12,10 @@ $leaky_paywall_settings(document).ready(function($) {
             $( 'td#issuem-leaky-paywall-restriction-rows table' ).append( response );
         });
 	});
-	
-	$( '#leaky_paywall_subscription_level_options' ).on( 'click', 'input#add-subscription-row-post-type', function( event ) {
-		event.preventDefault();
-		var row_key = $( this ).data( 'row-key' );
-		var select_post_key = 'leaky_paywall_subscription_row_' + row_key + '_last_post_type_key';
-        var data = {
-            'action': 'issuem-leaky-paywall-add-new-subscription-row-post-type',
-            'select-post-key': ++window[select_post_key],
-            'row-key': row_key,
-        }
-        $.post( ajaxurl, data, function( response ) {
-	        console.log( response );
-	        $('#issuem-leaky-paywall-subsciption-row-' + row_key + '-post-types table').append(response);
-        });
-	});
-	
-	$( '#issuem-leaky-paywall-subscription-level-rows' ).on( 'change', 'select.subscription_length_type', function( event ) {
-		var parent = $( this ).parent();
-		if ( 'unlimited' == $( this ).val() ) {
-			$( '.interval_count', parent ).data( 'prev-value', $( '.interval_count', parent ).val() )
-			$( '.interval_div', parent ).hide();
-			$( '.interval_count', parent ).val( '-1' );
-		} else {
-			$( '.interval_count', parent ).val( $( '.interval_count', parent ).data( 'prev-value' ) );
-			$( '.interval_div', parent ).show();
-		}
-	});
-	
-	$( '#issuem-leaky-paywall-subscription-level-rows' ).on( 'change', '.issuem-leaky-paywall-row-post-type select.allowed_type', function( event ) {
-		var parent = $( this ).parent();
-		if ( 'unlimited' == $( this ).val() ) {
-			$( '.allowed_value', parent ).data( 'prev-value', $( '.allowed_value', parent ).val() )
-			$( '.allowed_value_div', parent ).hide();
-			$( '.allowed_value', parent ).val( '-1' );
-		} else {
-			$( '.allowed_value', parent ).val( $( '.allowed_value', parent ).data( 'prev-value' ) );
-			$( '.allowed_value_div', parent ).show();
-		}
-	});
 		
 	$( '#leaky_paywall_default_restriction_options' ).on( 'click', '.delete-restriction-row', function ( event ) {
 		event.preventDefault();
 		var parent = $( this ).parents( '.issuem-leaky-paywall-restriction-row' );
-		parent.slideUp( 'normal', function() { $( this ).remove(); } );
-	});
-	
-	$( '#issuem-leaky-paywall-subscription-level-rows' ).on( 'click', '.delete-subscription-level', function ( event ) {
-		event.preventDefault();
-		var parent = $( this ).parents( '.issuem-leaky-paywall-subscription-level-row-table' );
-		parent.slideUp( 'normal', function() { $( this ).hide(); $( '.deleted-subscription', this ).val( 1 ); } );
-	});
-		
-	$( '#issuem-leaky-paywall-subscription-level-rows' ).on( 'click', '.delete-post-type-row', function ( event ) {
-		event.preventDefault();
-		var parent = $( this ).parent().parent( '.issuem-leaky-paywall-row-post-type' );
 		parent.slideUp( 'normal', function() { $( this ).remove(); } );
 	});
 
@@ -86,29 +35,6 @@ $leaky_paywall_settings(document).ready(function($) {
 
 		$.post( ajaxurl, data, function( response ) {
           	// build out the new select box
-			taxCell.append( response );
-			taxCell.find('.spinner').remove();
-        });
-
-	});
-
-
-	$( '.issuem-leaky-paywall-subscription-level-row-table').on( 'change', '.select_level_post_type', function() {
-		// replace taxonomy select with loader
-		var post_type = $(this).children("option:selected").val();
-		var taxCell = $(this).parent().next();
-		taxCell.append('<div class="spinner" style="visibility: visible; float: left;"></div>');
-		taxCell.find('select').remove();
-
-		// ajax call to find taxonomies and terms
-        var data = {
-            'action': 'leaky-paywall-get-restriction-row-post-type-taxonomies',
-            'post_type': post_type
-        }
-
-		$.post( ajaxurl, data, function( response ) {
-          	// build out the new select box
-          	taxCell.find('select').remove();
 			taxCell.append( response );
 			taxCell.find('.spinner').remove();
         });
@@ -139,14 +65,5 @@ $leaky_paywall_settings(document).ready(function($) {
 		
 	});
 
-	$('.stripe-recurring').click(function(e) {
-
-		if ( $(this).is(':checked' ) ) {
-			$(this).siblings('span').removeClass('hidden');
-		} else {
-			$(this).siblings('span').addClass('hidden');
-		}
-		
-	});
 
 });
